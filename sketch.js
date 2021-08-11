8f59691aedf1ec1e98f6941afc01ef82d59d8a5f
whitehatjr <rajeev@whitehatjr.com>
2021-08-11 11:20:38 +0530

    initial commit

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
-var engine, world, backgroundImg;
+var engine, world, backgroundImg,ground;
 
 
 
@@ -15,10 +15,18 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
   
+   var options = {
+    isStatic: true
+  }
+
+  ground = Bodies.rectangle(0, height - 1, width * 2, 1, options);
+  World.add(world, ground);
 }
 
 function draw() {
   background(189);
  
   Engine.update(engine);
+  
+   rect(ground.position.x, ground.position.y, width * 2, 1);
 }
